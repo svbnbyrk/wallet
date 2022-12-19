9ae675ec4a0c39544d296039ed39eb694b678389
Sevban Bayrak <svbnbyrk@gmail.com>
2022-12-19 09:39:41 +0300

    Add test user and wallet

--- internal/usecase/user_test.go
+++ internal/usecase/user_test.go
@@ -1,1 +1,64 @@
 package usecase_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/svbnbyrk/wallet/internal/entity"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+	"github.com/svbnbyrk/wallet/internal/usecase"
+)
+
+func User(t *testing.T) (*usecase.UserUseCase, *MockUserRepository) {
+	t.Helper()
+
+	mockCtl := gomock.NewController(t)
+	defer mockCtl.Finish()
+
+	user := NewMockUserRepository(mockCtl)
+
+	userUsecase := usecase.NewUserUseCase(user)
+
+	return userUsecase, user
+}
+
+func TestUserStore(t *testing.T) {
+	t.Parallel()
+
+	userUc, userRepo := User(t)
+
+	tests := []test{
+		{
+			name: "result with no error",
+			mock: func() {
+				userRepo.EXPECT().Store(context.Background(), entity.User{}).Return(nil)
+			},
+			res: nil,
+			err: nil,
+		},
+		{
+			name: "result with error",
+			mock: func() {
+				userRepo.EXPECT().Store(context.Background(), entity.User{}).Return(errInternalServErr)
+			},
+			res: entity.User{},
+			err: errInternalServErr,
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			tc.mock()
+
+			err := userUc.Store(context.Background(), entity.User{})
+
+			require.ErrorIs(t, err, tc.err)
+		})
+	}
+}
--- internal/usecase/wallet_test.go
+++ internal/usecase/wallet_test.go
@@ -1,1 +1,64 @@
 package usecase_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/svbnbyrk/wallet/internal/entity"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+	"github.com/svbnbyrk/wallet/internal/usecase"
+)
+
+func wallet(t *testing.T) (*usecase.WalletUseCase, *MockWalletRepository) {
+	t.Helper()
+
+	mockCtl := gomock.NewController(t)
+	defer mockCtl.Finish()
+
+	wallet := NewMockWalletRepository(mockCtl)
+
+	walletUsecase := usecase.NewWalletUseCase(wallet)
+
+	return walletUsecase, wallet
+}
+
+func TestWalletStore(t *testing.T) {
+	t.Parallel()
+
+	walletUc, walletRepo := wallet(t)
+
+	tests := []test{
+		{
+			name: "result with no error",
+			mock: func() {
+				walletRepo.EXPECT().Store(context.Background(), entity.Wallet{}).Return(nil)
+			},
+			res: nil,
+			err: nil,
+		},
+		{
+			name: "result with error",
+			mock: func() {
+				walletRepo.EXPECT().Store(context.Background(), entity.Wallet{}).Return(errInternalServErr)
+			},
+			res: entity.Wallet{},
+			err: errInternalServErr,
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			tc.mock()
+
+			err := walletUc.Store(context.Background(), entity.Wallet{})
+
+			require.ErrorIs(t, err, tc.err)
+		})
+	}
+}
